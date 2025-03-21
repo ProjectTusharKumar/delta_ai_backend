3b976010dde28490a53e03dee14c18fa039aa558
Tushar Kumar <160735794+ProjectTusharKumar@users.noreply.github.com>
2025-03-21 14:56:24 +0530

    Update server.py

--- server.py
+++ server.py
@@ -547,5 +547,6 @@ def get_employees():
 #         return jsonify({"status": "error", "message": f"Failed to upload data: {str(e)}"}), 500
 
 if __name__ == "__main__":
-   port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
